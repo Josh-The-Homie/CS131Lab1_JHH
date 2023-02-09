8af875d5603269983cb2e4c7d794dcea75473427
Josh-Henderson <122053780+Josh-The-Homie@users.noreply.github.com>
2023-02-09 12:23:25 -0600

    Update SportsTeam.java

--- CS131Lab1_JHH/Program2/SportsTeam.java
+++ CS131Lab1_JHH/Program2/SportsTeam.java
@@ -6,6 +6,9 @@ protected String teamMascot;
 protected String headCoach;
 protected int wins;
 protected int losses;
+/**
+ * Sets all String instance fields to "" and all integer instance fields to 0.
+ */
 public void Team() {
 teamName="";
 teamMascot="";
@@ -13,5 +16,23 @@ headCoach="";
 wins=0;
 losses=0;
 }//end Team
-
-}
+/**
+ * Sets all instance fields to 0.
+ * @param teamName
+ * @param teamMascot
+ * @param headCoach
+ */
+public void Team(String teamName, String teamMascot,String headCoach){
+	teamName="0";
+	teamMascot="0";
+	headCoach="0";
+	wins=0;
+	losses=0;}//end Team
+/**
+ * uses the formula wins/(wins + losses).
+ * @return Win percentage
+ */
+public double getWinPercentage() {
+	return wins/(wins+losses);}//end getWinPercentage
+public abstract double getStats();//end getStats
+}//end class
